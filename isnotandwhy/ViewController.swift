1fd92442500839fc9c08d8dc2c6ec9d1b6aa1efb
Denis Nascimento <denis.nascimento@m4u.com.br>
2018-10-09 09:52:24 -0300

    adding Speach class

--- isnotandwhy/ViewController.swift
+++ isnotandwhy/ViewController.swift
@@ -7,14 +7,109 @@
 //
 
 import UIKit
+import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,  SFSpeechRecognizerDelegate {
+    
+    @IBOutlet weak var textSpeach: UITextField!
+    
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "pt_BR"))!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    var speechResult = SFSpeechRecognitionResult()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        requestTranscribePermissions()
     }
+    
+    func requestTranscribePermissions() {
+      
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            // The callback may not be called on the main thread. Add an
+            // operation to the main queue to update the record button's state.
+            OperationQueue.main.addOperation {
+                var alertTitle = ""
+                var alertMsg = ""
+                
+                switch authStatus {
+                case .authorized:
+                    do {
+                        try self.startRecording()
+                    } catch {
+                        alertTitle = "Recorder Error"
+                        alertMsg = "There was a problem starting the speech recorder"
+                    }
+                    
+                case .denied:
+                    alertTitle = "Speech recognizer not allowed"
+                    alertMsg = "You enable the recgnizer in Settings"
+                    
+                case .restricted, .notDetermined:
+                    alertTitle = "Could not start the speech recognizer"
+                    alertMsg = "Check your internect connection and try again"
+                    
+                }
+                if alertTitle != "" {
+                    let alert = UIAlertController(title: alertTitle, message: alertMsg, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
+                        self.dismiss(animated: true, completion: nil)
+                    }))
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+        
+    }
+    
+    private func startRecording() throws {
+        if !audioEngine.isRunning {
 
+            try AVAudioSession.sharedInstance().setCategory(.record, mode: .measurement, options: [])
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+            
+            let inputNode = audioEngine.inputNode
+            guard let recognitionRequest = recognitionRequest else { fatalError("Unable to create the recognition request") }
+            
+            // Configure request so that results are returned before audio recording is finished
+            recognitionRequest.shouldReportPartialResults = true
+            
+            // A recognition task is used for speech recognition sessions
+            // A reference for the task is saved so it can be cancelled
+            recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+                var isFinal = false
+                
+                if let result = result {
+                    print("result: \(result.isFinal)")
+                    isFinal = result.isFinal
+                    self.textSpeach.text = result.bestTranscription.formattedString
+                }
+                
+                if error != nil || isFinal {
+                    self.audioEngine.stop()
+                    inputNode.removeTap(onBus: 0)
+                    
+                    self.recognitionRequest = nil
+                    self.recognitionTask = nil
+                }
+            }
+            
+            let recordingFormat = inputNode.outputFormat(forBus: 0)
+            inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+                self.recognitionRequest?.append(buffer)
+            }
+            audioEngine.prepare()
+            try audioEngine.start()
+
+        }
+        
+    }
 
 }
 
